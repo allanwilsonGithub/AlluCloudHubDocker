bcdf62355a985df55485dcbabcd05dc4acf9ba44
Allan Wilson <allan@allanwilson.net>
2020-04-13 23:59:14 +0300

    .

--- run_docker.sh
+++ run_docker.sh
@@ -2,9 +2,10 @@
 
 docker build -t allu-cloud-hub --build-arg CACHEBUST=$(date +%s) .
 
-docker run -d -t \
+docker run \
+    -d -t \
     --security-opt seccomp:unconfined \
     --shm-size "256M" \
     -p 3000:3000 \
     allu-cloud-hub \
-    /bin/bash -c "chmod 777 scripts/change_package_json_IP_to_local_IPV4.sh && scripts/change_package_json_IP_to_local_IPV4.sh && nohup npm run start"
+    /bin/bash -c "chmod 777 scripts/change_package_json_IP_to_local_IPV4.sh && scripts/change_package_json_IP_to_local_IPV4.sh && npm start"
