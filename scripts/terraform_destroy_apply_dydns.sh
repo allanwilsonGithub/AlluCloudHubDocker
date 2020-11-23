f019054cf16ed13104a9e0b344f8ca6383a2c22e
Allan Wilson <allan@allanwilson.net>
2020-11-24 01:36:35 +0200

    www.allanwilson.net baby!

--- scripts/terraform_destroy_apply_dydns.sh
+++ scripts/terraform_destroy_apply_dydns.sh
@@ -4,4 +4,4 @@ terraform apply -auto-approve
 # Set www3.allanwilson.net:3000 to the new public IP address using Dynamic DNS
 ALLUCLOUD_IP_OUTPUT=$(terraform output)
 ALLUCLOUD_IP=${ALLUCLOUD_IP_OUTPUT:5}
-wget http://directnic.com/dns/gateway/68825a3ee333d4426d68d0f9eb0c9ee6de6a26cee8fc90d26e0acb2219f64c58/?data=$ALLUCLOUD_IP
+wget http://directnic.com/dns/gateway/37892b84c11234183990cad367c4dc18914901d5560c75e56bc9f809e671fcb2/?data=$ALLUCLOUD_IP
